e3c82f5329640b252e9a1b2524e98d7cf7ddd9f6
bobomurod <bobomurod.boboev@yandex.ru>
2018-05-10 19:36:35 +0300

    to be contd

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const request = require('request')
 
-request('https://api.coinmarketcap.com/v2/ticker/1/', function(error, response, bosy){
+request('https://api.coinmarketcap.com/v2/ticker/1/', function(error, response, body){
     if (!error && response.statusCode == 200) {
         var jsonArr = JSON.parse(body);
         console.log(jsonArr.data.quotes.USD.price)
